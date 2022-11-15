91e63d9856bc2995a8fabe9ee6fa360e09f35286
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2022-11-15 14:58:06 -0500

    terser_and_copy_to_extensions_folder.mjs

---
+++ build/terser_and_copy_to_extensions_folder.mjs
@@ -0,0 +1,121 @@
+import fs from 'fs'
+import { minify } from "terser";
+
+const folderName = "undefined.my-bracket-select-0.0.1"
+
+async function main() {
+    let targetFolder
+    if (process.platform === "win32") {
+        targetFolder = `${process.env.USERPROFILE}/.vscode/extensions/${folderName}`
+    } else {
+        targetFolder = `~/.vscode/extensions/${folderName}`
+    }
+    if ( !fs.existsSync( targetFolder ) ) {
+        fs.mkdirSync( targetFolder )
+    }
+
+    const obj = JSON.parse(fs.readFileSync("package.json").toString())
+    fs.writeFileSync(`${targetFolder}/package.json`,JSON.stringify({
+        name: obj.name,
+        version: obj.version,
+        engines: obj.engines,
+        activationEvents: obj.activationEvents,
+        main: obj.main,
+        contributes: obj.contributes,
+    }))
+
+    const input = fs.readFileSync("extension.js").toString()
+
+    // const result1 = await minify(input, {
+    //     compress: {
+    //         drop_console: true,
+    //     }
+    // })
+    const result2 = await minify(input, {
+    // const result2 = await minify(input.replace(/let ([^=]*[,;])+|let |const /g, ""), {
+    // const result2 = await minify(input.replace(/let /g, "var "), {
+        compress: {
+            arguments: true,
+            arrows: true,
+            booleans_as_integers: true,
+            booleans: true,
+            collapse_vars: true,
+            comparisons: true,
+            computed_props: true,
+            conditionals: true,
+            dead_code: true,
+            defaults: true,
+            directives: true,
+            drop_console: true,
+            drop_debugger: true,
+            ecma: 2020,
+            evaluate: true,
+            expression: true,
+            hoist_funs: true,
+            hoist_props: true,
+            hoist_vars: true,
+            ie8: true,
+            if_return: true,
+            inline: true,
+            join_vars: true,
+            keep_classnames: true,
+            keep_fargs: true,
+            keep_fnames: true,
+            keep_infinity: true,
+            loops: true,
+            module: true,
+            negate_iife: true,
+            passes: 2,
+            properties: true,
+            pure_getters: true,
+            reduce_funcs: true,
+            reduce_vars: true,
+            sequences: true,
+            side_effects: true,
+            switches: true,
+            toplevel: true,
+            top_retain: [],
+            typeofs: true,
+            unsafe_arrows: true,
+            unsafe: true,
+            unsafe_comps: true,
+            unsafe_Function: true,
+            unsafe_math: true,
+            unsafe_symbols: true,
+            unsafe_methods: true,
+            unsafe_proto: true,
+            unsafe_regexp: true,
+            unsafe_undefined: true,
+            unused: true,
+        },
+        mangle: {
+            eval: true,
+            keep_classnames: false,
+            keep_fnames: false,
+            module: true,
+            reserved: [],
+            toplevel: true,
+        },
+        toplevel: true,
+    })
+    // console.log(result1.code.length)
+    // console.log(result1.code.replace(/let ([^=]*[,;])+|let |const /g, "").length)
+    // console.log(result2.code.length)
+
+    // console.log(`${targetFolder}/extension.js`)
+
+    // fs.writeFileSync(`${targetFolder}/extension.js`, result.code)
+    // fs.writeFileSync(`${targetFolder}/extension.js`, result2.code.replace(/let ([^=]*[,;])+|let |const /g, ""))
+    // fs.writeFileSync(`${targetFolder}/extension.js`, result1.code.replace(/let ([^=]*[,;])+|let |const /g, ""))
+    // fs.writeFileSync(`${targetFolder}/extension.js`, result2.code.replace(/varz ([^=]*[,;])+|var |const /g, ""))
+    // fs.writeFileSync(`${targetFolder}/extension.js`, result2.code.replace(/const /g, ""))
+    // const output1 = result2.code.replace(/const /g, "")
+    const output2 = result2.code.replace(/const /g, "let ")
+    // console.log(input.length)
+    // console.log(output1.length)
+    // console.log(output2.length)
+    fs.writeFileSync(`${targetFolder}/extension.js`, output2)
+    // fs.writeFileSync(`${targetFolder}/extension.js`, result1.code)
+
+}
+main()
--- extension.js
+++ extension.js
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-const {getGetRanges, searchRange} = require('./rangesLogic')
+const vscode = require('vscode')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,7 +12,7 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "my-bracket-select" is now active!');
+	console.log('Congratulations, your extension "my-bracket-select" is now active!')
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
@@ -66,15 +65,583 @@ function activate(context) {
 
 		// Display a message box to the user
 		// vscode.window.showInformationMessage('Hello World from my-bracket-select!');
-	});
+	})
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
 	deactivate
+}
+
+/**
+ * @param {string} languageId
+ */
+function getGetRanges(languageId) {
+	switch (languageId) {
+		case "ahk":
+			/**
+			 * @param {string} str
+			 */
+			return function getRanges(str) {
+				let c = 0
+				const len = str.length
+
+				let childArr = []
+				const stack = []
+
+				while (c < len) {
+					switch (str[c]) {
+						// strings
+						case '"': {
+							const startIndex = c
+							c++
+							const execArray = /^(?:""|[^"])*"/.exec(str.slice(c))
+							if (!execArray) {
+								continue
+							}
+							const lastIndex = execArray[0].length + c
+							childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+							c = lastIndex
+							continue
+						}
+						case '%': {
+							const startIndex = c
+							c++
+							const execArray = /^[_a-zA-Z][_a-zA-Z0-9]*?%/.exec(str.slice(c))
+							if (!execArray) {
+								continue
+							}
+							const lastIndex = execArray[0].length + c
+							childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+							c = lastIndex
+							continue
+						}
+						// comments
+						case '/':
+							c++
+							switch (str[c]) {
+								case '/': {
+									const lastIndex = /.*$/m.exec(str.slice(c))[0].length + c
+									c = lastIndex
+									continue
+								}
+								case '*': {
+									const lastIndex = /[\s\S]*?\*\//.exec(str.slice(c))[0].length + c
+									c = lastIndex
+									continue
+								}
+								default: {
+									continue
+								}
+							}
+						case ";":
+							c++
+							if (c === 1 || str[c - 2] === ' ' || str[c - 2] === '\n' || str[c - 2] === '\t') {
+								const lastIndex = /.*$/m.exec(str.slice(c))[0].length + c
+								c = lastIndex
+							}
+							continue
+						// brackets
+						case '(':
+						case '[':
+						case '{':
+							const tempChildArr = []
+							childArr.push([c, null, tempChildArr, c + 1, null, c, null])
+							stack.push(childArr)
+							childArr = tempChildArr
+							c++
+							continue
+						case ')':
+						case ']':
+						case '}':
+							c++
+							if (!stack.length) {
+								console.log(`more ")" than "(": ${c}`)
+								continue
+							}
+							childArr = stack.pop()
+							childArr[childArr.length - 1][1] = c
+							childArr[childArr.length - 1][4] = c - 1
+							childArr[childArr.length - 1][6] = c
+							continue
+						default:
+							c++
+							continue
+					}
+				}
+
+				return childArr
+			}
+		case "ahk2":
+			/**
+			 * @param {string} str
+			 */
+			return function getRanges(str) {
+				let c = 0
+				const len = str.length
+
+				let childArr = []
+				const stack = []
+
+				while (c < len) {
+					switch (str[c]) {
+						// strings
+						case '"': {
+							const startIndex = c
+							c++
+							const execArray = /^(?:`.|.)*?"/.exec(str.slice(c))
+							if (!execArray) {
+								continue
+							}
+							const lastIndex = execArray[0].length + c
+							childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+							c = lastIndex
+							continue
+						}
+						case '\'': {
+							const startIndex = c
+							c++
+							const execArray = /^(?:`.|.)*?'/.exec(str.slice(c))
+							if (!execArray) {
+								continue
+							}
+							const lastIndex = execArray[0].length + c
+							childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+							c = lastIndex
+							continue
+						}
+						case '%': {
+							const startIndex = c
+							c++
+							const execArray = /^[_a-zA-Z][_a-zA-Z0-9]*?%/.exec(str.slice(c))
+							if (!execArray) {
+								continue
+							}
+							const lastIndex = execArray[0].length + c
+							childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+							c = lastIndex
+							continue
+						}
+						// comments
+						case '/':
+							c++
+							switch (str[c]) {
+								case '/': {
+									const lastIndex = /.*$/m.exec(str.slice(c))[0].length + c
+									c = lastIndex
+									continue
+								}
+								case '*': {
+									const lastIndex = /[\s\S]*?\*\//.exec(str.slice(c))[0].length + c
+									c = lastIndex
+									continue
+								}
+								default: {
+									continue
+								}
+							}
+						case ";":
+							c++
+							if (c === 1 || str[c - 2] === ' ' || str[c - 2] === '\n' || str[c - 2] === '\t') {
+								const lastIndex = /.*$/m.exec(str.slice(c))[0].length + c
+								c = lastIndex
+							}
+							continue
+						// brackets
+						case '(':
+						case '[':
+						case '{':
+							const tempChildArr = []
+							childArr.push([c, null, tempChildArr, c + 1, null, c, null])
+							stack.push(childArr)
+							childArr = tempChildArr
+							c++
+							continue
+						case ')':
+						case ']':
+						case '}':
+							c++
+							if (!stack.length) {
+								console.log(`more ")" than "(": ${c}`)
+								continue
+							}
+							childArr = stack.pop()
+							childArr[childArr.length - 1][1] = c
+							childArr[childArr.length - 1][4] = c - 1
+							childArr[childArr.length - 1][6] = c
+							continue
+						default:
+							c++
+							continue
+					}
+				}
+
+				return childArr
+			}
+		case "zig":
+			/**
+			 * @param {string} str
+			 */
+			return function getRanges(str) {
+				let c = 0
+				const len = str.length
+
+				let childArr = []
+				const stack = []
+
+				while (c < len) {
+					switch (str[c]) {
+						// strings
+						case '"': {
+							const startIndex = c
+							c++
+							const execArray = /^(?:\\.|.)*?"/.exec(str.slice(c))
+							if (!execArray) {
+								continue
+							}
+							const lastIndex = execArray[0].length + c
+							childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+							c = lastIndex
+							continue
+						}
+						case '\'': {
+							const startIndex = c
+							c++
+							const execArray = /^(?:\\.|.)*?'/.exec(str.slice(c))
+							if (!execArray) {
+								continue
+							}
+							const lastIndex = execArray[0].length + c
+							childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+							c = lastIndex
+							continue
+						}
+						case '|': {
+							const startIndex = c
+							c++
+							const execArray = /^.*?\|/.exec(str.slice(c))
+							if (!execArray) {
+								continue
+							}
+							const lastIndex = execArray[0].length + c
+							childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+							c = lastIndex
+							continue
+						}
+						// comments
+						case '/':
+							c++
+							switch (str[c]) {
+								case '/': {
+									const lastIndex = /.*$/m.exec(str.slice(c))[0].length + c
+									c = lastIndex
+									continue
+								}
+								default: {
+									continue
+								}
+							}
+						// brackets
+						case '(':
+						case '[':
+						case '{':
+							const tempChildArr = []
+							childArr.push([c, null, tempChildArr, c + 1, null, c, null])
+							stack.push(childArr)
+							childArr = tempChildArr
+							c++
+							continue
+						case ')':
+						case ']':
+						case '}':
+							c++
+							if (!stack.length) {
+								console.log(`more ")" than "(": ${c}`)
+								continue
+							}
+							childArr = stack.pop()
+							childArr[childArr.length - 1][1] = c
+							childArr[childArr.length - 1][4] = c - 1
+							childArr[childArr.length - 1][6] = c
+							continue
+						default:
+							c++
+							continue
+					}
+				}
+
+				return childArr
+			}
+		default: //default is "js":
+			/**
+			 * @param {string} str
+			 */
+			return function getRanges(str) {
+				let c = 0
+				const len = str.length
+
+				let childArr = []
+				const stack = []
+
+				let mode = 0 //0: "normal", 1: "inside template literal", 2: "inside ${}"
+				let isDiv = false
+				outer:
+				while (true) {
+					switch (mode) {
+						case 0:
+						case 2:
+							while (c < len) {
+								switch (str[c]) {
+									// strings
+									case '"': {
+										const startIndex = c
+										c++
+										const execArray = /^(?:\\.|.)*?"/.exec(str.slice(c))
+										if (!execArray) {
+											continue
+										}
+										const lastIndex = execArray[0].length + c
+										childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+										c = lastIndex
+										continue
+									}
+									case '\'': {
+										const startIndex = c
+										c++
+										const execArray = /^(?:\\.|.)*?'/.exec(str.slice(c))
+										if (!execArray) {
+											continue
+										}
+										const lastIndex = execArray[0].length + c
+										childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+										c = lastIndex
+										continue
+									}
+									case '`': {
+										const tempChildArr = []
+										const tempArr = [c, null, tempChildArr, c + 1, null, c, null, mode]
+										childArr.push(tempArr)
+										stack.push(childArr)
+										childArr = tempChildArr
+										c++
+
+										mode = 1
+										continue outer
+									}
+									// comments
+									case '/':
+										c++
+										switch (str[c]) {
+											case '/': {
+												const lastIndex = /.*$/m.exec(str.slice(c))[0].length + c
+												c = lastIndex
+												continue
+											}
+											case '*': {
+												const lastIndex = /[\s\S]*?\*\//.exec(str.slice(c))[0].length + c
+												c = lastIndex
+												continue
+											}
+											default: {
+												if (isDiv) {
+													continue
+												}
+												//regex
+												const execArray = /^(?:\\.|.)*?\//.exec(str.slice(c))
+												if (!execArray) {
+													continue
+												}
+												const startIndex = c - 1
+												const lastIndex = execArray[0].length + c
+												childArr.push([startIndex, lastIndex, [], startIndex + 1, lastIndex - 1, startIndex, lastIndex])
+												c = lastIndex
+												continue
+											}
+										}
+									// brackets
+									case '(':
+									case '[':
+										isDiv = false
+										const tempChildArr = []
+										const tempArr = [c, null, tempChildArr, c + 1, null, c, null]
+										childArr.push(tempArr)
+										stack.push(childArr)
+										childArr = tempChildArr
+										c++
+										continue
+									case '{': {
+										const tempChildArr = []
+										const tempArr = [c, null, tempChildArr, c + 1, null, c, null, 0] //tempArr[7] is mode
+										childArr.push(tempArr)
+										stack.push(childArr)
+										childArr = tempChildArr
+										c++
+										continue
+									}
+									case ')':
+									case ']':
+										c++
+										if (!stack.length) {
+											console.log(`more ")" than "(": ${c}`)
+											continue
+										}
+										childArr = stack.pop()
+										childArr[childArr.length - 1][1] = c
+										childArr[childArr.length - 1][4] = c - 1
+										childArr[childArr.length - 1][6] = c
+										continue
+									case '}':
+										c++
+										if (!stack.length) {
+											console.log(`more ")" than "(": ${c}`)
+											continue
+										}
+										childArr = stack.pop()
+										childArr[childArr.length - 1][1] = c
+										childArr[childArr.length - 1][4] = c - 1
+										childArr[childArr.length - 1][6] = c
+										if (childArr[childArr.length - 1][7] !== mode) {
+											mode = childArr[childArr.length - 1][7]
+											if (mode === 1) {
+												if (!stack.length) {
+													console.log(`more ")" than "(": ${c}`)
+													continue outer
+												}
+												childArr = stack.pop()
+												childArr[childArr.length - 1][1] = c
+												childArr[childArr.length - 1][4] = c
+												childArr[childArr.length - 1][6] = c
+											}
+											continue outer
+										}
+										continue
+									case " ":
+									case "\t":
+									case "\n":
+										c++
+										continue
+									case "=":
+									case "&":
+									case "|":
+									case "?":
+									case ":":
+									case "!":
+									case ",":
+									case ";":
+										c++
+										isDiv = false
+										continue
+									default:
+										c++
+										isDiv = true
+										continue
+								}
+							}
+							break
+						case 1:
+							while (c < len) {
+								switch (str[c]) {
+									case '$':
+										if (c + 1 < len && str[c + 1] === "{") {
+											const tempChildArr = []
+											const tempArr = [c, null, tempChildArr, c, null, c, null, true] //tempArr[7] is mode
+											childArr.push(tempArr)
+											stack.push(childArr)
+											childArr = tempChildArr
+											{
+												c++
+												const tempChildArr = []
+												const tempArr = [c, null, tempChildArr, c + 1, null, c - 1, null, 1] //tempArr[7] is mode
+												childArr.push(tempArr)
+												stack.push(childArr)
+												childArr = tempChildArr
+											}
+											c++
+											mode = 0
+											continue outer
+										}
+									case '`':
+										c++
+										if (!stack.length) {
+											console.log(`more ")" than "(": ${c}`)
+											continue
+										}
+										childArr = stack.pop()
+										childArr[childArr.length - 1][1] = c
+										childArr[childArr.length - 1][4] = c - 1
+										childArr[childArr.length - 1][6] = c
+										mode = childArr[childArr.length - 1][7]
+
+										continue outer
+									case '\\':
+										c += 2
+										continue
+									default:
+										c++
+										continue
+								}
+							}
+							break
+					}
+					break
+				}
+
+
+
+				return childArr
+			}
+	}
+
+
+
+}
+
+function searchRange(ranges, smaller, bigger) {
+	if (!ranges.length) {
+		return false
+	}
+
+	let childArr = ranges
+	let low = 0
+	let high = childArr.length - 1
+	let mid = Math.trunc((low + high) / 2)
+	let validArr = false
+
+	while (true) {
+		if (smaller < childArr[mid][0]) {
+			//search left
+			if (low === high) {
+				break
+			}
+			high = mid
+			mid = Math.trunc((low + high) / 2)
+
+		} else if (smaller > childArr[mid][0] && bigger < childArr[mid][1]) {
+			validArr = childArr[mid]
+			if (!validArr[2].length) {
+				break
+			}
+			childArr = validArr[2]
+			// again
+			low = 0
+			high = childArr.length - 1
+			mid = Math.trunc((low + high) / 2)
+		} else if (smaller > childArr[mid][1]) {
+			//search right
+			if (low === high) {
+				break
+			}
+			low = mid + 1
+			mid = Math.trunc((low + high) / 2)
+		} else {
+			break
+		}
+	}
+
+	return validArr
 }
